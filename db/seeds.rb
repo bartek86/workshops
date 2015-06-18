89ec1ca93de46b57a9593707bcead5e35a97812a
morihito <bartek.lesniak86@gmail.com>
2015-06-18 20:13:51 +0000

    seed create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# user seed
 5.times do
     User.create(
         email: Faker::Internet.email,
@@ -15,10 +16,38 @@
     )
 end
 
+#admin seed
 User.create(
     email: 'admin@example.com',
     password: 'admin123',
     firstname: 'The',
     lastname: 'Admin',
     admin: true)
-    
+
+# category seed    
+3.times do
+   Category.create(
+       name: Faker::Product.brand
+    )
+end
+
+# product seed
+30.times do
+    Product.create(
+        title: Faker::Product.product,
+        description: Faker::Lorem.paragraph,
+        price: rand(1..100) + rand.round(2),
+        category_id: rand(1..3),
+        user_id: rand(1..5)
+    )
+end
+
+#Review seed
+300.times do
+    Review.create(
+        content: Faker::Lorem.paragraph,
+        rating: rand(1..5),
+        product_id: rand(1..30),
+        user_id: rand(1..5)
+        )
+end        
